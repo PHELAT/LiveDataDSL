b932256ecd9fabf6fb0f4fd30787c3768881802b
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-01-21 18:51:21 +0330

    Add ability to generate a extended class with the required constructor

--- livedatadsl-processor/src/main/java/com/phelat/livedatadsl/processor/Processor.kt
+++ livedatadsl-processor/src/main/java/com/phelat/livedatadsl/processor/Processor.kt
@@ -1,6 +1,12 @@
 package com.phelat.livedatadsl.processor
 
 import com.phelat.livedatadsl.LiveData
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeName
+import com.squareup.javapoet.TypeSpec
+import java.io.IOException
 import java.util.*
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.Filer
@@ -9,8 +15,12 @@ import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.element.AnnotationMirror
 import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
+import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
+import javax.tools.Diagnostic
 
 class Processor : AbstractProcessor() {
 
@@ -32,10 +42,65 @@ class Processor : AbstractProcessor() {
     ): Boolean {
         if (!roundEnvironment.processingOver()) {
             for (typeElement in getTypeElements(roundEnvironment.rootElements, annotations)) {
-                // TODO : generate
+                processTypeElement(typeElement)
             }
         }
         return true
+    }
+
+    private fun processTypeElement(typeElement: TypeElement) {
+        val packageName = elementUtils.getPackageOf(typeElement).qualifiedName.toString()
+        val typeName = typeElement.simpleName.toString()
+        val className = ClassName.get(packageName, typeName)
+        val generatedClass = ClassName.get(packageName, "${typeName}_DSL")
+
+        val classBuilder = TypeSpec.classBuilder(generatedClass).apply {
+            addModifiers(Modifier.PUBLIC)
+            superclass(className)
+            generateBodyOfClass(typeElement, this)
+        }
+
+        try {
+            JavaFile.builder(packageName, classBuilder.build())
+                .build()
+                .writeTo(filer)
+        } catch (e: IOException) {
+            messager.printMessage(Diagnostic.Kind.ERROR, e.toString(), typeElement)
+        }
+    }
+
+    private fun generateBodyOfClass(typeElement: TypeElement, classBuilder: TypeSpec.Builder) {
+        val enclosedElements = typeElement.enclosedElements
+        enclosedElements.forEachIndexed { index, element ->
+            if (ElementKind.CONSTRUCTOR == element.kind) {
+                generateConstructor(element, classBuilder)
+            }
+        }
+    }
+
+    private fun generateConstructor(element: Element, classBuilder: TypeSpec.Builder) {
+        val constructor = element as ExecutableElement
+        val constructorParameters = constructor.parameters
+
+        val constructorBuilder = MethodSpec.constructorBuilder().apply {
+            addModifiers(constructor.modifiers)
+            val superCall = StringBuilder("super(")
+            for (i in constructorParameters.indices) {
+                val modifiers = constructorParameters[i].modifiers
+                addParameter(
+                    TypeName.get(constructorParameters[i].asType()),
+                    constructorParameters[i].simpleName.toString(),
+                    *modifiers.toTypedArray()
+                )
+                superCall.append(constructorParameters[i].simpleName.toString())
+                if (i < constructorParameters.size - 1) {
+                    superCall.append(", ")
+                }
+            }
+            superCall.append(");")
+            addCode(superCall.toString())
+        }
+        classBuilder.addMethod(constructorBuilder.build())
     }
 
     private fun getTypeElements(
